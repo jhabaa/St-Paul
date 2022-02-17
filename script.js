d7a62a0695357114574401d803a8e67d0c1ca716
Jean Hubert ABA'A <78347379+jhabaa@users.noreply.github.com>
2022-02-17 16:43:05 +0100

    v0.4

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function toUs(){
     window.open("us.html");
 }
 function showdate(){
+    var months = ["Janvier","Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"];
     const date = new Date();
-    document.getElementById("date").innerHTML = date.getFullYear();
+    document.getElementById("date").innerHTML = months[date.getMonth()] + " "+ date.getFullYear();
 }
